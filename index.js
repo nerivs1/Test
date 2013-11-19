4df05774d6fce230cfdae79c7f3934b583798489
nerivs1 <neri@videostudio1.com>
2013-11-19 18:48:17 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,47 +1,24 @@
-(function($) {
-    var methods = {
-        init : function(options) {
-            var settings = {
-                callback: function() {}
-            };
-
-            if ( options ) {
-                $.extend( settings, options );
-                }
-
-            $(":jqmData(role='page')").each(function() {
-                $(this).bind("swiperight", function() {
-                    var nextPage = parseInt($(this).attr("id").split("page")[1]) - 1;
-                    if (nextPage === 0) 
-                        nextPage = 3;
-
-                    $.mobile.changePage("#page"+nextPage, "slide");
-                    });                        
-
-                $(this).bind("swipeleft", function() {
-                    var nextPage = parseInt($(this).attr("id").split("page")[1]) +1;
-                    if (nextPage === 4) 
-                        nextPage = 1;
-
-                    $.mobile.changePage("#page"+nextPage, "slide");
-                });
-            })
+$(document).on('swipeleft', '[data-role="page"]', function(event){    
+    if(event.handled !== true) // This will prevent event triggering more then once
+    {    
+        var nextpage = $(this).next('[data-role="page"]');
+        // swipe using id of next page if exists
+        if (nextpage.length > 0) {
+            $.mobile.changePage(nextpage, {transition: "slide", reverse: false}, true, true);
         }
-        }
-
-    $.fn.initApp = function(method) {
-        if ( methods[method] ) {
-            return methods[method].apply(this, Array.prototype.slice.call(arguments, 1));
-        } 
-        else if ( typeof method === 'object' || ! method ) {
-            return methods.init.apply( this, arguments );
-        } 
-        else {
-            $.error( 'Method ' + method + ' does not exist' );
-        }
+        event.handled = true;
     }
-    })(jQuery);
+    return false;         
+});
 
-$(document).ready(function(){
-    $().initApp();
+$(document).on('swiperight', '[data-role="page"]', function(event){   
+    if(event.handled !== true) // This will prevent event triggering more then once
+    {      
+        var prevpage = $(this).prev('[data-role="page"]');
+        if (prevpage.length > 0) {
+            $.mobile.changePage(prevpage, {transition: "slide", reverse: true}, true, true);
+        }
+        event.handled = true;
+    }
+    return false;            
 });
